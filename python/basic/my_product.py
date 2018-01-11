f5753a263eb83c073a313b2c85f16703bfd9e8dc
watarucn <ximier@gmail.com>
2018-01-11 20:07:04 +0800

    product
    complete the function of product

--- python/basic/my_product.py
+++ python/basic/my_product.py
@@ -1,15 +1,20 @@
 # -*- coding : utf -8 -*-
 import math
 
-def product(*numbers):
 
-	r = 1
+def product(*numbers):
+	
+	for test in numbers:
+		if not isinstance(test,(int,float)):
+			raise TypeError ("请输入数字")
+		
 	l = len(numbers)
 	if l == 0:
-		return None
+		raise TypeError ("请输入数字")
 	elif l == 1:
-		return numbers
+		return numbers[0]
 	else:
+		r = 1
 		l = l -1
 		while l >= 0:
 			r = r*numbers[l]
