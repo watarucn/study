c96f95a1c62feb1911aede86e0b433bbfc391760
watarucn <ximier@gmail.com>
2018-01-17 23:58:48 +0800

    修正了错误

--- c/strindex.c
+++ c/strindex.c
@@ -37,8 +37,9 @@ int strindex(char s[],char t[])
 	
 	for (i=0;s[i]!='\0';i++){
 		for(j=i,k=0;t[k]!='\0'&& s[j]==t[k];j++,k++)
-			if(k>0 && t[k]=='\0')
-				return i;
+			;
+	if(k>0 && t[k]=='\0')
+		return i;
 	}
 	return -1;
 }
