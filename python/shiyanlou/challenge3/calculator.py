be5a168b2aca69daef46a8139c1c47530bc38545
ximier <ximier@gmail.com>
2018-02-04 20:36:21 +0800

    add UserData's attr

--- python/shiyanlou/challenge3/calculator.py
+++ python/shiyanlou/challenge3/calculator.py
@@ -18,7 +18,10 @@ class Config (object):
 
         for line in file.readlines():
             temp = line.split('=')
-            config[temp[0].strip()]=temp[1].strip()
+            try:
+                config[temp[0].strip()]=int(temp[1].strip())
+            except:
+                print("Parament Error")
         
         return config
 
@@ -34,9 +37,11 @@ class Config (object):
 
 class UserData(object):
     def __init__(self,userdatafile):
-        self._config = self._read_config(userdatafile)
+        self._data = self._read_data(userdatafile)
+        self.ID = self._data.keys()
+        self.Income = self._data.values()
 
-    def _read_config(self,filename):
+    def _read_data(self,filename):
         try:
             file=open(filename)
         except:
@@ -47,11 +52,56 @@ class UserData(object):
         data={}
         for line in flie.readlines():
             temp = line.split(',')
-            data[temp[0]]=temp[1]
+            try:            
+                data[int(temp[0])]=float(temp[1])
+            except:
+                print("Paramen Error")
+
+    def calc(self,JishuL,JishuH,SocialTaxRate):
+        result=[]
+        for n in range(len(self.ID)):
+
+           if self.Income[n]<JishuL:
+               Money = JishuL
+           elif self.Income[n]>JishuH:
+               Money = JishuH
+           else:
+               Money = self.Income[n]
+
+           SocialTax = Money * SocialTaxRate
+           IncomeSubSocial = self.Income[n]-SocialTax
+           if IncomeSubSocial < 3500:
+               TaxableIncome = 0
+           else:
+               TaxableIncome = IncomeSubSocial -3500
+
+           if TaxableIncome >80000:
+               Tax = TaxableIncome * 0.45 -13505
+           elif TaxableIncome >55000:
+               Tax = TaxableIncome * 0.35 -5505
+           elif TaxableIncome >35000:
+               Tax = TaxableIncome * 0.30 -2755
+           elif TaxableIncome >9000:
+               Tax = TaxableIncome * 0.25 -1005
+           elif TaxableIncome >4500:
+               Tax = TaxableIncome * 0.20 -555
+           elif TaxableIncome >1500:
+               Tax = TaxableIncome * 0.10 -105
+           else:
+               Tax = TaxableIncome * 0.03
+
+           IncomeSubTax = IncomeSubSocial - Tax
+           
+           result.append('%d,%d,%2.f,%2.f,%2.f,'%(self.ID[n],self.Income[n],SocialTax,Tax,IncomeSubTax)
+
+        return result       
 
 if __name__ == '__main__':
 
     config =Config(sys.argv[1])
-    print(config.get_config('JiShuL'))
+    user = UserData(sys.argv[2])
+    money = user.calc(config.get_config(JishuL),config.get_config(JishuH),config.get_SocialTax)
+    for x in money:    
+        print(x)
            
         
